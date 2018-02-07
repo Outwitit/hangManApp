8ab6c5486f2ffd4a56ba66b07ee6e3d9a31ee8f1
Charles  Buckley <charlesbuckley@admins-MacBook-Pro-2.local>
2018-02-07 18:47:55 -0500

    commit message

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m',
-				'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m',
+// 				'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 
+var phrase = "reach for the sky";
+var phraseArray = ['r','e','a','c','h','f','o','t','s','k','y']
