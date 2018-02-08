0ed6322a531ca9ce0e213d28f6d66fe9d277707a
Charles  Buckley <charlesbuckley@admins-MacBook-Pro-2.local>
2018-02-07 19:08:30 -0500

    commit message

--- game.js
+++ game.js
@@ -1,22 +1,40 @@
 // var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m',
 // 				'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+window.onload = function(){
+	var phrase = "reach for the sky";
+	var phraseArray = ['r','e','a','c','h','f','o','t','s','k','y'];
+	var lives;
+	var counter;
 
-var phrase = "reach for the sky";
-var phraseArray = ['r','e','a','c','h','f','o','t','s','k','y']
+
+	// When page loads
+	var ready = prompt("Ready to play? yes/no");
+	if(ready === "yes"){
+		
+	} else {
+		alert("Don't be a weenie. It's game time.");
+	}
+
+	//When user presses any letter key 
 
 
-// When page loads
-var ready = prompt("Ready to play? yes/no");
-if(ready === "yes"){
-	
-} else {
-	alert("Don't be a weenie. It's game time.");
+
+	// User lives
+	var showLives = document.getElementById("mylives");
+	// Show lives
+	comments = function () {
+	showLives.innerHTML = "You have " + lives + " lives";
+	if (lives < 1) {
+	  showLives.innerHTML = "Game Over";
+	}
+	for (var i = 0; i < geusses.length; i++) {
+	  if (counter + space === geusses.length) {
+	    showLives.innerHTML = "You Win!";
+	  }
+	}
+	}
 }
-
-//When user presses any letter key 
-
-
 
 
 
