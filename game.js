2d83c97245a168e07b9e12f5ac8ea9c536dd5fcb
Charles  Buckley <charlesbuckley@admins-MacBook-Pro-2.local>
2018-02-07 19:31:16 -0500

    message

--- game.js
+++ game.js
@@ -1,22 +1,10 @@
-// var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m',
-// 				'n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 window.onload = function(){
 	var phrase = "reach for the sky";
-	var phraseArray = ['r','e','a','c','h','f','o','t','s','k','y'];
+	// var phraseArray = ['r','e','a','c','h','f','o','t','s','k','y'];
 	var lives;
 	var counter;
-
-
-	// When page loads
-	var ready = prompt("Ready to play? yes/no");
-	if(ready === "yes"){
-		
-	} else {
-		alert("Don't be a weenie. It's game time.");
-	}
-
-	//When user presses any letter key 
+	var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m',
+ 					'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 
 
@@ -34,6 +22,84 @@ window.onload = function(){
 	  }
 	}
 	}
+
+    // Animate man
+    var animate = function () {
+      var drawMe = lives ;
+      drawArray[drawMe]();
+    }
+    // Hangman
+  canvas =  function(){
+
+    myStickman = document.getElementById("stickman");
+    context = myStickman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = "#fff";
+    context.lineWidth = 2;
+  };
+  
+    head = function(){
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+    }
+    
+  draw = function(pathFromx, pathFromy, pathTox, pathToy) {
+    
+    context.moveTo(pathFromx, pathFromy);
+    context.lineTo(pathTox, pathToy);
+    context.stroke(); 
+	}
+
+	frame1 = function() {
+     draw (0, 150, 150, 150);
+   };
+   
+   frame2 = function() {
+     draw (10, 0, 10, 600);
+   };
+  
+   frame3 = function() {
+     draw (0, 5, 70, 5);
+   };
+  
+   frame4 = function() {
+     draw (60, 5, 60, 15);
+   };
+  
+   torso = function() {
+     draw (60, 36, 60, 70);
+   };
+  
+   rightArm = function() {
+     draw (60, 46, 100, 50);
+   };
+  
+   leftArm = function() {
+     draw (60, 46, 20, 50);
+   };
+  
+   rightLeg = function() {
+     draw (60, 70, 100, 100);
+   };
+  
+   leftLeg = function() {
+     draw (60, 70, 20, 100);
+   };
+  
+  var drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+
+
+  
+
+
+
+
+
+
+
 }
 
 
